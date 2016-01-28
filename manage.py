d109dc9e7110f55def4bd4fab57e8b35d0245696
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-01-28 12:18:42 +0100

    manage: Add coala subcommand

--- manage.py
+++ manage.py
@@ -4,11 +4,29 @@
 This is an application management application that allows to run the server.
 """
 
+from subprocess import call
+
 from flask_script import Manager
 
 from server.app import app
 
 manager = Manager(app)
+
+
+@manager.command
+def coala(noninteractive=False):
+    """
+    Runs code analysis on the application.
+
+    :param noninteractive: True if no interactions are to be done. (JSON
+    output will be generated in this case.)
+    :return: 0 if all is well.
+    """
+    if noninteractive:
+        return call(['coala-format', '-S', 'format_str={origin}, {file}, from '
+                                           '{line} to {end_line}. {message}'])
+    else:
+        return call('coala')
 
 
 if __name__ == '__main__':
